9e2cdc29ac085c381320c3f18ea155dca7b15067
Alex Sorafumo <alex@yuion.net>
2019-12-24 09:25:54 +1100

    fix(logs): fix log colours

--- projects/library/src/lib/settings.ts
+++ projects/library/src/lib/settings.ts
@@ -17,7 +17,7 @@ export const LIB_NAME = 'Spinners';
 export function log(type: string, msg: string, args?: any, out: string = 'debug', color?: string) {
     if (window.debug) {
         const clr = color ? color : '#009688';
-        const COLOURS = ['color: #0288D1', `color:${clr}`, 'color:rgba(0,0,0,0.87)'];
+        const COLOURS = ['color: #0288D1', `color:${clr}`, 'color:default'];
         if (args) {
             if (hasColours()) {
                 console[out](`%c[${LIB_NAME}]%c[${type}] %c${msg}`, ...COLOURS, args);
